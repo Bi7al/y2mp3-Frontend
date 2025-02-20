c05a985e222a20a865aa9eb854108d1648729049
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-02-20 22:07:25 +0500

    Added URL Encoder

--- Assets/javascripts/script.js
+++ Assets/javascripts/script.js
@@ -1,7 +1,13 @@
 document.getElementById('convertBtn').addEventListener('click', async function (event) {
     event.preventDefault();
     var input = document.getElementById('url').value;
-    let response = await fetch(`https://y2mp3-backend.vercel.app/download/${input}`);
+    const encodedUrl = encodeURIComponent(input);
+    let response = await fetch(`https://y2mp3-backend.vercel.app/download/${encodedUrl}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
     let data = await response.json();
     console.log(data);
 })
