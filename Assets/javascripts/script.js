4ddef5ccb20ced4acce6417e340ec9088448b6b5
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-02-20 21:43:27 +0500

    Added JAVASCRIPT

--- Assets/javascripts/script.js
+++ Assets/javascripts/script.js
@@ -1,1 +1,7 @@
-
+document.getElementById('convertBtn').addEventListener('click', async function (event) {
+    event.preventDefault();
+    var input = document.getElementById('url').value;
+    let response = await fetch(`https://y2mp3-backend.vercel.app/download/${input}`);
+    let data = await response.json();
+    console.log(data);
+})
